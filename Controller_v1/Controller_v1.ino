190be3f4f15b77389b108ae6ec9522ae6419a4c8
Unknown <Tylerquacken@gmail.com>
2018-03-29 12:55:02 -0600

    Auto stash before merge of "master" and "Laser_Battle_Bots/master"

--- Controller_v1/Controller_v1.ino
+++ Controller_v1/Controller_v1.ino
@@ -192,19 +192,10 @@ void loop() {
       ammo[weapon_select]--;
       weapon_cooldown = millis() + cooldown_times[weapon_select];
     }
-  
-    ////////GET INCOMING////////
 //    // Listen for the incoming response if transmission is acknowledged
 //    radio.startListening();
-//    unsigned long timeout = millis() + 10;    //10ms timeout
-//    //while((!radio.available()) && (millis < timeout));    //wait until trans received
-//    delay(10);
-//    if(radio.available())    //if not timed out, get message
-//      radio.read(&incoming, sizeof(incoming));
-//
-//    radio.stopListening();
-//
-//    ////////ACT ON INCOMING & OUTGOING////////
+//    while(!radio.available());
+//    radio.read(&incoming, sizeof(incoming));
 //    
 //    //decode incoming data
 //    //{hpMSbyte, hpLSbyte, 0b[~,~,~,~,~,slow,disable,freeze]}
@@ -212,21 +203,21 @@ void loop() {
 //    hpMS << 8;
 //    int hpLS = incoming[1];
 //    hp = hpMS + hpLS;
-//    //Serial.println(hp);
-    
+//
+//    radio.stopListening();
   }
 
   
-  Serial.print(message[0]);
-  Serial.print("\t");
-  Serial.print(message[1]);
-  Serial.print("\t");
-  Serial.print(bitRead(message[2],0));
-  Serial.print(bitRead(message[2],1));
-  Serial.print(bitRead(message[2],2));
-  Serial.print(selectl);
-  Serial.print(selectr);
-  Serial.println("");
+//  Serial.print(message[0]);
+//  Serial.print("\t");
+//  Serial.print(message[1]);
+//  Serial.print("\t");
+//  Serial.print(bitRead(message[2],0));
+//  Serial.print(bitRead(message[2],1));
+//  Serial.print(bitRead(message[2],2));
+//  Serial.print(selectl);
+//  Serial.print(selectr);
+//  Serial.println("");
 
     
   // Check weapon select
@@ -294,3 +285,28 @@ void printVitals(){
 }
 
 
+
+  
+    ////////GET INCOMING////////
+//    // Listen for the incoming response if transmission is acknowledged
+//    unsigned long timeout = millis() + 10;    //10ms timeout
+//    //while((!radio.available()) && (millis < timeout));    //wait until trans received
+//    delay(10);
+//    if(radio.available())    //if not timed out, get message
+//      radio.read(&incoming, sizeof(incoming));
+//
+//    radio.stopListening();
+//
+//    ////////ACT ON INCOMING & OUTGOING////////
+//    //Serial.println(hp);
+    
+  Serial.print(message[0]);
+  Serial.print("\t");
+  Serial.print(message[1]);
+  Serial.print("\t");
+  Serial.print(bitRead(message[2],0));
+  Serial.print(bitRead(message[2],1));
+  Serial.print(bitRead(message[2],2));
+  Serial.print(selectl);
+  Serial.print(selectr);
+  Serial.println("");
